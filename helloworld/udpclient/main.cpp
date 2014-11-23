ebee4ed859959d39a531c545eefea01a710364b9
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-11-23 17:13:02 +0000

    First functional example of UDP in RAW mode (no RTOS)
    Implemented by setting a receive callback on the socket class.

--- helloworld/udpclient/main.cpp
+++ helloworld/udpclient/main.cpp
@@ -2,50 +2,52 @@
 #include "EthernetInterface.h"
 #include "test_env.h"
 
-// namespace {
-//     const char *HTTP_SERVER_NAME = "utcnist.colorado.edu";
-//     const int HTTP_SERVER_PORT = 37;
-//     const float YEARS_TO_PASS = 114.0;
-// }
+namespace {
+     //const char *HTTP_SERVER_NAME = "utcnist.colorado.edu";
+     const char *HTTP_SERVER_NAME = "128.138.140.44";
+     const int HTTP_SERVER_PORT = 37;
+     const float YEARS_TO_PASS = 114.0;
+}
 
+static volatile bool received = false;
+static bool result = false;
 
-int main() {
-    bool result = false;
-    EthernetInterface eth;
-    printf("Starting.\r\n");
-    eth.init(); //Use DHCP
-    eth.connect();
-    printf("UDP client IP Address is %s\n", eth.getIPAddress());
-#if 0
-    UDPSocket sock;
-    sock.init();
-
-    Endpoint nist;
-    nist.set_address(HTTP_SERVER_NAME, HTTP_SERVER_PORT);
-
-    char out_buffer[] = "plop"; // Does not matter
-    sock.sendTo(nist, out_buffer, sizeof(out_buffer));
-
+void on_recv(const uint8_t* p, uint16_t n, Endpoint& remote) {
     union {
         char in_buffer_tab[4];
         unsigned int in_buffer_uint;
     };
 
-    const int n = sock.receiveFrom(nist, in_buffer_tab, sizeof(in_buffer_tab));
-    if (n > 0) {
-        result = true;
-        const unsigned int timeRes = ntohl(in_buffer_uint);
-        const float years = timeRes / 60.0 / 60.0 / 24.0 / 365;
-        printf("UDP: Received %d bytes from server %s on port %d\r\n", n, nist.get_address(), nist.get_port());
-        printf("UDP: %u seconds since 01/01/1900 00:00 GMT ... %s\r\n", timeRes, timeRes > 0 ? "[OK]" : "[FAIL]");
-        printf("UDP: %.2f years since 01/01/1900 00:00 GMT ... %s\r\n", years, timeRes > YEARS_TO_PASS ? "[OK]" : "[FAIL]");
+    memcpy(in_buffer_tab, p, 4);
 
-        if (years < YEARS_TO_PASS) {
-            result = false;
-        }
-    }
+    result = true;
+    const unsigned int timeRes = ntohl(in_buffer_uint);
+    const float years = timeRes / 60.0 / 60.0 / 24.0 / 365;
+    printf("UDP: Received %d bytes from server %s on port %d\r\n", n, remote.get_address(), remote.get_port());
+    printf("UDP: %u seconds since 01/01/1900 00:00 GMT ... %s\r\n", timeRes, timeRes > 0 ? "[OK]" : "[FAIL]");
+    printf("UDP: %.2f years since 01/01/1900 00:00 GMT ... %s\r\n", years, timeRes > YEARS_TO_PASS ? "[OK]" : "[FAIL]");
+    received = true;
+}
+
+int main() {
+    EthernetInterface eth;
+    printf("Starting.\r\n");
+    eth.init(); //Use DHCP
+    eth.connect();
+    printf("UDP client IP Address is %s\n", eth.getIPAddress());
+
+    UDPSocket sock;
+    sock.init();
+    sock.setReceiveCallback(on_recv);
+
+    Endpoint nist;
+    nist.set_address(HTTP_SERVER_NAME, HTTP_SERVER_PORT);
+    printf("Connecting to: %s\n", nist.get_address());
+    char out_buffer[] = "plop"; // Does not matter
+    sock.sendTo(nist, out_buffer, sizeof(out_buffer));
+    while (!received);
+
     sock.close();
-#endif
     eth.disconnect();
     notify_completion(result);
     return 0;
