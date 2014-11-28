905a769192494069f6091d48c393601006e79dee
Brendan Moran <brendan.moran@arm.com>
2014-11-28 12:02:32 +0000

    Update UDP HelloWorld client. Add a class to manage time server connections.

--- helloworld/udpclient/main.cpp
+++ helloworld/udpclient/main.cpp
@@ -1,33 +1,80 @@
 #include "mbed.h"
 #include "EthernetInterface.h"
 #include "test_env.h"
+#include "UDPaSocket.h"
+
+#define UDP_TIME_PORT 37
 
 namespace {
      //const char *HTTP_SERVER_NAME = "utcnist.colorado.edu";
      const char *HTTP_SERVER_NAME = "128.138.140.44";
-     const int HTTP_SERVER_PORT = 37;
      const float YEARS_TO_PASS = 114.0;
 }
 
-static volatile bool received = false;
-static bool result = false;
+using std::memset;
+using std::memcpy;
 
-void on_recv(const uint8_t* p, uint16_t n, Endpoint& remote) {
-    union {
-        char in_buffer_tab[4];
-        unsigned int in_buffer_uint;
-    };
+class UDPGetTime {
+public:
+    UDPGetTime() :
+            sock(_default_irq.entry() ), txBuf("foo"), _udpTimePort(UDP_TIME_PORT)
+    {
+        inBuf.buffer = rxBuf;
+        inBuf.length = sizeof(rxBuf);
+        inBuf.pos    = 0;
+        outBuf.buffer = txBuf;
+        outBuf.length = sizeof(txBuf);
+        outBuf.pos    = 0;
 
-    memcpy(in_buffer_tab, p, 4);
+        _default_irq.callback(&defaultHandler);
+        _send_irq.callback(&sendHandler);
+        _recv_irq.callback(&recvHandler);
 
-    result = true;
-    const unsigned int timeRes = ntohl(in_buffer_uint);
-    const float years = timeRes / 60.0 / 60.0 / 24.0 / 365;
-    printf("UDP: Received %d bytes from server %s on port %d\r\n", n, remote.get_address(), remote.get_port());
-    printf("UDP: %u seconds since 01/01/1900 00:00 GMT ... %s\r\n", timeRes, timeRes > 0 ? "[OK]" : "[FAIL]");
-    printf("UDP: %.2f years since 01/01/1900 00:00 GMT ... %s\r\n", years, timeRes > YEARS_TO_PASS ? "[OK]" : "[FAIL]");
-    received = true;
-}
+    }
+    uint32_t getTime(char *address) {
+        ip_addr_t addr;
+        if (!ipaddr_aton(HTTP_SERVER_NAME,addr)) {
+            defaultHandler(0);
+            return 0;
+        }
+
+        received = false;
+        // Zero the buffer memory
+        memset(inBuf.buffer,0,inBuf.length);
+        sock.start_recv(inBuf, 0, _recv_irq.entry());
+        sock.start_send_to(addr,  outBuf, 0, _send_irq.entry());
+        while(!received) { __WFI(); }
+
+        uint32_t time;
+        // Correct for possible non 32-bit alignment
+        memcpy(&time, rxBuf, sizeof(time));
+        // Switch to host order
+        time = ntohl(time);
+        return time;
+    }
+protected:
+    void defaultHandler(void* arg) {
+        printf("Caught a default event\n");
+    }
+    void sendHandler(void *arg) {
+        printf("message sent\n");
+    }
+    void recvHandler(void *arg) {
+        received = true;
+    }
+protected:
+    UDPaSocket sock;
+    volatile bool received;
+    const uint16_t _udpTimePort;
+protected:
+    buffer_t inBuf;
+    buffer_t outBuf;
+    volatile char rxBuf[32];
+    char txBuf[32];
+    CThunk<UDPGetTime> _default_irq;
+    CThunk<UDPGetTime> _send_irq;
+    CThunk<UDPGetTime> _recv_irq;
+};
 
 int main() {
     EthernetInterface eth;
@@ -35,19 +82,16 @@ int main() {
     eth.init(); //Use DHCP
     eth.connect();
     printf("UDP client IP Address is %s\n", eth.getIPAddress());
+    UDPGetTime gt;
+    printf("Connecting to: %s:%d\n", HTTP_SERVER_NAME, UDP_TIME_PORT);
+    uint32_t timeRes = gt.getTime(HTTP_SERVER_NAME);
 
-    UDPSocket sock;
-    sock.init();
-    sock.setReceiveCallback(on_recv);
 
-    Endpoint nist;
-    nist.set_address(HTTP_SERVER_NAME, HTTP_SERVER_PORT);
-    printf("Connecting to: %s\n", nist.get_address());
-    char out_buffer[] = "plop"; // Does not matter
-    sock.sendTo(nist, out_buffer, sizeof(out_buffer));
-    while (!received);
+    const float years = timeRes / 60.0 / 60.0 / 24.0 / 365;
+    printf("UDP: Received %d bytes from server %s on port %d\r\n", n, remote.get_address(), remote.get_port());
+    printf("UDP: %u seconds since 01/01/1900 00:00 GMT ... %s\r\n", timeRes, timeRes > 0 ? "[OK]" : "[FAIL]");
+    printf("UDP: %.2f years since 01/01/1900 00:00 GMT ... %s\r\n", years, timeRes > YEARS_TO_PASS ? "[OK]" : "[FAIL]");
 
-    sock.close();
     eth.disconnect();
     notify_completion(result);
     return 0;
