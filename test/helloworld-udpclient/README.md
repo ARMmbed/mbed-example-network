7b82c796a4ad96c2ae62bba24b8624997804d0ed
Brendan Moran <brendan.moran@arm.com>
2015-03-11 17:15:24 +0000

    Add comments and README.md to helloworld-udpclient

---
+++ test/helloworld-udpclient/README.md
@@ -0,0 +1,74 @@
+# UDP Time Example
+This application reads the current UTC time by sending a packet to utcnist.colorado.edu (128.138.140.44)
+
+This example is implemented as a logic class (UDPGetTime) wrapping a UDP socket. The logic class handles all events, leaving the main loop to just check if the process has finished.
+
+## Pre-requisites
+To build and run this example the requirements below are necessary:
+* A computer with the following software installed
+** CMake
+** yotta
+** python
+** arm gcc toolchain
+** pyOCD
+** a serial terminal emulator (e.g. screen)
+* A frdm-k64f development board
+* An ethernet connection to the internet
+* An ethernet cable
+* A micro-USB cable
+
+## Getting Started
+1. Connect the frdm-k64f to the internet using the ethernet cable
+2. Connect the frdm-k64f to the computer with the micro-USB cable, being careful to use the micro-usb port labled "OpenSDA"
+3. Check out a copy of mbed-example-network
+4. Open a terminal in the root mbed-example-network directory
+5. Update all the dependencies of mbed-example-network
+```
+$ yt up
+```
+6. Check that there are no missing dependencies
+```
+$ yt ls
+```
+7. Build the examples. This will take a long time if it is the first time that the examples have been built.
+```
+$ yt build
+```
+8. Start the serial terminal emulator and connect to the virtual serial port presented by frdm-k64f.
+9. Open a new terminal, then start the pyOCD GDB server.
+```
+$ python pyOCD/test/gdb_server.py
+```
+The output should look like this:
+```
+Welcome to the PyOCD GDB Server Beta Version 
+INFO:root:new board id detected: 02400201B1130E4E4CXXXXXX
+id => usbinfo | boardname
+0 => MB MBED CM (0xd28, 0x204) [k64f]
+INFO:root:DAP SWD MODE initialised
+INFO:root:IDCODE: 0x2BA01477
+INFO:root:K64F not in secure state
+INFO:root:6 hardware breakpoints, 4 literal comparators
+INFO:root:4 hardware watchpoints
+INFO:root:CPU core is Cortex-M4
+INFO:root:FPU present
+INFO:root:GDB server started at port:3333
+```
+10. Switch back to the first terminal window, then start GDB and connect to the GDB server.
+```
+$ arm-none-eabi-gdb -ex "target remote localhost:3333" -ex load ./build/frdm-k64f-gcc/test/mbed-example-network-test-helloworld-udpclient
+```
+11. Once the program has loaded, start it.
+```
+(gdb) c
+```
+12. The output in the terminal window should look like:
+```
+Starting.
+Running eth.connect()
+UDP client IP Address is 10.2.202.167
+Connecting to: 128.138.140.44:37
+UDP: 1426093904 seconds since 01/01/1900 00:00 GMT ... [OK]
+UDP: 45.22 years since 01/01/1900 00:00 GMT ... [OK]
+```
+13. The LED should blink slowly (about 0.5Hz)
--- test/helloworld-udpclient/main.cpp
+++ test/helloworld-udpclient/main.cpp
@@ -2,6 +2,15 @@
  * PackageLicenseDeclared: Apache-2.0
  * Copyright 2015 ARM Holdings PLC
  */
+/** \file main.cpp
+ *  \brief An example UDP Time application
+ *  This application reads the current UTC time by sending a packet to
+ *  utcnist.colorado.edu (128.138.140.44)
+ *
+ *  This example is implemented as a logic class (UDPGetTime) wrapping a UDP socket.
+ *  The logic class handles all events, leaving the main loop to just check if the process
+ *  has finished.
+ */
 #include "mbed.h"
 #include "EthernetInterface.h"
 #include "socket_types.h"
@@ -23,8 +32,21 @@ namespace {
      const float YEARS_TO_PASS = 114.0;
 }
 
+/**
+ * \brief UDPGetTime implements the logic for fetching the UTC time over UDP
+ */
 class UDPGetTime {
 public:
+	/**
+	 * UDPGetTime Constructor
+	 * Initializes the UDP socket, sets up event handlers and flags.
+	 *
+	 * Note that CThunk is used for event handlers.  This will be changed to a C++
+	 * function pointer in an upcoming release.
+	 *
+	 * @param[in] stack The stack to use for the UDP Socket.  Currently,
+	 * only SOCKET_STACK_LWIP_IPV4 is supported
+	 */
     UDPGetTime(const socket_stack_t stack) :
         sock(stack),
         _udpTimePort(UDP_TIME_PORT),
@@ -32,41 +54,77 @@ public:
         _recv_irq(this),
         _dns_irq(this)
 	{
+    	/* Initialize callbacks for events */
         _recv_irq.callback(&UDPGetTime::onRecv);
         _dns_irq.callback(&UDPGetTime::onDNS);
+        /* Open the UDP socket so that DNS will work */
         sock.open(SOCKET_AF_INET4);
     }
+    /**
+     * Initiate the get time operation
+     * Starts by resolving the address (optionally with DNS)
+     * @param[in] address The address from which to query the time
+     * @return SOCKET_ERROR_NONE on success, or an error code on failure
+     */
     socket_error_t startGetTime(const char *address) {
         socket_error_t err;
+        /* Initialize the GetTime flags */
         resolved = false;
         received = false;
-        memset(&_resolvedAddr,0,sizeof(_resolvedAddr));
-        err = sock.resolve(address,(handler_t)_dns_irq.entry());
-		return err;
+        /* Start the DNS operation */
+        return sock.resolve(address,(handler_t)_dns_irq.entry());
     }
+    /**
+     * Check if the UDP time response has been received
+     * @return true if the time response has been received, false otherwise.
+     */
     bool isReceived() {return received;}
+    /**
+     * Gets the time response.
+     * Once the response has been received from the remote host, getTime() can be used
+     * to extract the time from UDPGetTime
+     * @return The 32-bit time since 1970-01-01T00:00:00 in seconds.
+     */
     uint32_t getTime() { return _time;}
 protected:
+    /**
+     * The DNS Response Handler
+     * @param[in] arg (unused)
+     */
     void onDNS(void * arg) {
         (void) arg;
+        /* Extract the Socket event to read the resolved address */
         socket_event_t *event = sock.getEvent(); // TODO: (CThunk upgrade/Alpha2)
         _resolvedAddr.setAddr(&event->i.d.addr);
+        /* Register the read handler */
         sock.setOnReadable((handler_t)_recv_irq.entry());
-        sock.send_to("foo",strlen("foo"),&_resolvedAddr,_udpTimePort);
-        // TODO: Add a timeout?
+        /* Send the query packet to the remote host */
+        socket_error_t err = sock.send_to("time",strlen("time"),&_resolvedAddr,_udpTimePort);
+        /* A failure on send is a fatal error in this example */
+    	if (err != SOCKET_ERROR_NONE) {
+    		printf("Socket Error %d\r\n", err);
+    	    notify_completion(false);
+    	}
     }
+    /**
+     * The Time Query response handler
+     * @param[in] arg (unused)
+     */
     void onRecv(void *arg) {
     	(void) arg;
+    	/* Initialize the buffer size */
     	size_t nRx = sizeof(_rxBuf);
+    	/* Receive some bytes */
     	socket_error_t err = sock.recv(_rxBuf, &nRx);
+        /* A failure on recv is a fatal error in this example */
     	if (err != SOCKET_ERROR_NONE) {
     		printf("Socket Error %d\r\n", err);
     	    notify_completion(false);
     	}
         uint32_t time;
-        // Correct for possible non 32-bit alignment
+        /* Correct for possible non 32-bit alignment */
         memcpy(&time, _rxBuf, sizeof(time));
-        // Switch to host order
+        /* Switch to host order */
         _time = ntohl(time);
     	received = true;
     }
@@ -81,9 +139,10 @@ protected:
 
 protected:
     char _rxBuf[32];
-
-    // These should not be CThunk.  We will remove CThunk from this class once
-    // we decide on the function pointer format we will use for event handlers
+    /*
+	 * Note that CThunk is used for event handlers.  This will be changed to a C++
+	 * function pointer in an upcoming release.
+	 */
     CThunk<UDPGetTime> _recv_irq;
     CThunk<UDPGetTime> _dns_irq;
 };
@@ -104,10 +163,13 @@ int main() {
     printf("Connecting to: %s:%d\r\n", HTTP_SERVER_NAME, UDP_TIME_PORT);
     gt.startGetTime(HTTP_SERVER_NAME);
 
+    /* Wait until the get time process has completed */
     while (!gt.isReceived()) { __WFI(); }
 
+    /* Get the time */
     uint32_t timeRes = gt.getTime();
 
+    /* Check that the response is sane */
     const float years = timeRes / 60.0 / 60.0 / 24.0 / 365;
     //printf("UDP: Received %d bytes from server %s on port %d\r\n", n, remote.get_address(), remote.get_port());
     printf("UDP: %lu seconds since 01/01/1900 00:00 GMT ... %s\r\n", timeRes, timeRes > 0 ? "[OK]" : "[FAIL]");
