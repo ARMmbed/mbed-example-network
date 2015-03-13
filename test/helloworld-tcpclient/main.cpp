fd03e01472dc1dd5e7f001cdb1ee8d515e6a2d33
Brendan Moran <brendan.moran@arm.com>
2015-03-13 13:44:55 +0000

    Clean up and simplify network examples

--- test/helloworld-tcpclient/main.cpp
+++ test/helloworld-tcpclient/main.cpp
@@ -16,7 +16,6 @@
 #include "TCPStream.h"
 #include "test_env.h"
 
-// TODO: Remove when yotta supports init.
 #include "lwipv4_init.h"
 
 namespace {
@@ -27,7 +26,7 @@ const int RECV_BUFFER_SIZE = 600;
 const char HTTP_PATH[] = "/media/uploads/mbed_official/hello.txt";
 const size_t HTTP_PATH_LEN = sizeof(HTTP_PATH) - 1;
 
-// Test related data
+/* Test related data */
 const char *HTTP_OK_STR = "200 OK";
 const char *HTTP_HELLO_STR = "Hello world!";
 }
@@ -38,194 +37,173 @@ const char *HTTP_HELLO_STR = "Hello world!";
  */
 class HelloHTTP {
 public:
-	/**
-	 * HelloHTTP Constructor
-	 * Initializes the TCP socket, sets up event handlers and flags.
-	 *
-	 * Note that CThunk is used for event handlers.  This will be changed to a C++
-	 * function pointer in an upcoming release.
-	 *
-	 *
-	 * @param[in] domain The domain name to fetch from
-	 * @param[in] port The port of the HTTP server
-	 * @param[in] stack The stack to use for the UDP Socket.  Currently,
-	 * only SOCKET_STACK_LWIP_IPV4 is supported
-	 * @param[in] localPort The optional local port to bind. This can be useful for servers
-	 * with long keep-alive so that repeated invokations of HelloHTTP work when the other end
-	 * of the connection has not been closed. Changing the port will force the remote server to
-	 * interpret the connection attempt as a new connection.
-	 */
-	HelloHTTP(const char * domain, const uint16_t port, const socket_stack_t stack, uint16_t localPort = 0) :
-			_stream(stack), _domain(domain), _port(port), _connect(this), _receive(this), _resolved(this)
-	{
-		_connect.callback(&HelloHTTP::onConnect);
-		_receive.callback(&HelloHTTP::onReceive);
-		_resolved.callback(&HelloHTTP::onDNS);
+    /**
+     * HelloHTTP Constructor
+     * Initializes the TCP socket, sets up event handlers and flags.
+     *
+     * Note that CThunk is used for event handlers.  This will be changed to a C++
+     * function pointer in an upcoming release.
+     *
+     *
+     * @param[in] domain The domain name to fetch from
+     * @param[in] port The port of the HTTP server
+     */
+    HelloHTTP(const char * domain, const uint16_t port) :
+            _stream(SOCKET_STACK_LWIP_IPV4), _domain(domain), _port(port), _connect(this), _receive(this), _resolved(this)
+    {
+        _connect.callback(&HelloHTTP::onConnect);
+        _receive.callback(&HelloHTTP::onReceive);
+        _resolved.callback(&HelloHTTP::onDNS);
 
-		_error = false;
-		_gothello = false;
-		_got200 = false;
-		_bpos = 0;
-		_stream.open(SOCKET_AF_INET4);
-		if (localPort) {
-			_stream.bind("0.0.0.0", localPort);
-		}
-	}
-	/**
-	 * Initiate the test.
-	 *
-	 * Starts by clearing test flags, then resolves the address with DNS.
-	 *
-	 * @param[in] path The path of the file to fetch from the HTTP server
+        _error = false;
+        _gothello = false;
+        _got200 = false;
+        _bpos = 0;
+        _stream.open(SOCKET_AF_INET4);
+    }
+    /**
+     * Initiate the test.
+     *
+     * Starts by clearing test flags, then resolves the address with DNS.
+     *
+     * @param[in] path The path of the file to fetch from the HTTP server
      * @return SOCKET_ERROR_NONE on success, or an error code on failure
-	 */
-	int startTest(const char *path) {
-		/* Initialize the flags */
-		_got200 = false;
-		_gothello = false;
-		_error = false;
-		/* Fill the request buffer */
-		_bpos = snprintf(_buffer, sizeof(_buffer) - 1, "GET %s HTTP/1.1\nHost: %s\n\n", path, HTTP_SERVER_NAME);
+     */
+    int startTest(const char *path) {
+        /* Initialize the flags */
+        _got200 = false;
+        _gothello = false;
+        _error = false;
+        /* Fill the request buffer */
+        _bpos = snprintf(_buffer, sizeof(_buffer) - 1, "GET %s HTTP/1.1\nHost: %s\n\n", path, HTTP_SERVER_NAME);
 
-		/* Connect to the server */
-		printf("Connecting to %s:%d\n", _domain, _port);
-		/* Resolve the domain name: */
-		socket_error_t err = _stream.resolve(_domain,
-				(handler_t) (void *) _resolved.entry());
-		return err;
-	}
-	/**
-	 * Check if the test has completed.
-	 * @return Returns true if done, false otherwise.
-	 */
-	bool done() {
-		return _error || (_got200 && _gothello);
-	}
-	/**
-	 * Check if there was an error
-	 * @return Returns true if there was an error, false otherwise.
-	 */
-	bool error() {
-		return _error;
-	}
-	/**
-	 * Closes the TCP socket
-	 */
-	void close() {
-		_stream.close();
-	}
+        /* Connect to the server */
+        printf("Connecting to %s:%d\n", _domain, _port);
+        /* Resolve the domain name: */
+        socket_error_t err = _stream.resolve(_domain,
+                (handler_t) (void *) _resolved.entry());
+        return err;
+    }
+    /**
+     * Check if the test has completed.
+     * @return Returns true if done, false otherwise.
+     */
+    bool done() {
+        return _error || (_got200 && _gothello);
+    }
+    /**
+     * Check if there was an error
+     * @return Returns true if there was an error, false otherwise.
+     */
+    bool error() {
+        return _error;
+    }
+    /**
+     * Closes the TCP socket
+     */
+    void close() {
+        _stream.close();
+    }
 protected:
-	/**
-	 * On Connect handler
-	 * Sends the request which was generated in startTest
-	 */
-	void onConnect() {
-		//Send the request
-		handler_t h = (handler_t) _receive.entry(); // TODO: CThunk replacement/Alpha3
-		_stream.setOnReadable(h);
-		socket_error_t rc = _stream.send(_buffer, _bpos); // No onSent needed
-		if (rc != SOCKET_ERROR_NONE) {
-			_error = true;
-		}
-	}
-	/**
-	 * On Receive handler
-	 * Parses the response from the server, to check for the HTTP 200 status code and the expected response ("Hello World!")
-	 */
-	void onReceive() {
-		_bpos = sizeof(_buffer);
-		/* Read data out of the socket */
-		socket_error_t err = _stream.recv(_buffer, &_bpos);
-		if (err != SOCKET_ERROR_NONE) {
-			_error = true;
-			return;
-		}
-		_buffer[_bpos] = 0;
-		/* Check each of the flags */
-		_got200 = _got200 || strstr(_buffer, HTTP_OK_STR) != NULL;
-		_gothello = _gothello || strstr(_buffer, HTTP_HELLO_STR) != NULL;
-		/* Print status messages */
-		printf("HTTP: Received %d chars from server\r\n", _bpos);
-		printf("HTTP: Received 200 OK status ... %s\r\n", _got200 ? "[OK]" : "[FAIL]");
-		printf("HTTP: Received '%s' status ... %s\r\n", HTTP_HELLO_STR, _gothello ? "[OK]" : "[FAIL]");
-		printf("HTTP: Received message:\r\n\r\n");
-		printf("%s", _buffer);
-	}
-	/**
-	 * On DNS Handler
-	 * Reads the address returned by DNS, then starts the connect process.
-	 */
-	void onDNS() {
-		handler_t h = (handler_t) _connect.entry(); // TODO: CThunk replacement/Alpha3
-		socket_event_t *e = _stream.getEvent();
-		/* Check that the result is a valid DNS response */
-		if (e->i.d.addr.type == SOCKET_STACK_UNINIT) {
-			//Could not find DNS entry
-			_error = true;
-			printf("Could not find DNS entry for %s", HTTP_SERVER_NAME);
-			return;
-		} else {
-			/* Start connecting to the remote host */
-			_remoteAddr.setAddr(&e->i.d.addr);
-			socket_error_t err = _stream.connect(&_remoteAddr, _port, h);
-			if (err != SOCKET_ERROR_NONE) {
-				_error = true;
-			}
-		}
-	}
+    /**
+     * On Connect handler
+     * Sends the request which was generated in startTest
+     */
+    void onConnect() {
+        /* Send the request */
+        handler_t h = (handler_t) _receive.entry();
+        _stream.setOnReadable(h);
+        socket_error_t rc = _stream.send(_buffer, _bpos);
+        if (rc != SOCKET_ERROR_NONE) {
+            _error = true;
+        }
+    }
+    /**
+     * On Receive handler
+     * Parses the response from the server, to check for the HTTP 200 status code and the expected response ("Hello World!")
+     */
+    void onReceive() {
+        _bpos = sizeof(_buffer);
+        /* Read data out of the socket */
+        socket_error_t err = _stream.recv(_buffer, &_bpos);
+        if (err != SOCKET_ERROR_NONE) {
+            _error = true;
+            return;
+        }
+        _buffer[_bpos] = 0;
+        /* Check each of the flags */
+        _got200 = _got200 || strstr(_buffer, HTTP_OK_STR) != NULL;
+        _gothello = _gothello || strstr(_buffer, HTTP_HELLO_STR) != NULL;
+        /* Print status messages */
+        printf("HTTP: Received %d chars from server\r\n", _bpos);
+        printf("HTTP: Received 200 OK status ... %s\r\n", _got200 ? "[OK]" : "[FAIL]");
+        printf("HTTP: Received '%s' status ... %s\r\n", HTTP_HELLO_STR, _gothello ? "[OK]" : "[FAIL]");
+        printf("HTTP: Received message:\r\n\r\n");
+        printf("%s", _buffer);
+    }
+    /**
+     * On DNS Handler
+     * Reads the address returned by DNS, then starts the connect process.
+     */
+    void onDNS() {
+        handler_t h = (handler_t) _connect.entry();
+        socket_event_t *e = _stream.getEvent();
+        /* Check that the result is a valid DNS response */
+        if (e->i.d.addr.type == SOCKET_STACK_UNINIT) {
+            /* Could not find DNS entry */
+            _error = true;
+            printf("Could not find DNS entry for %s", HTTP_SERVER_NAME);
+            return;
+        } else {
+            /* Start connecting to the remote host */
+            _remoteAddr.setAddr(&e->i.d.addr);
+            socket_error_t err = _stream.connect(&_remoteAddr, _port, h);
+            if (err != SOCKET_ERROR_NONE) {
+                _error = true;
+            }
+        }
+    }
 
 protected:
-	TCPStream _stream;              /**< The TCP Socket */
-	const char *_domain;            /**< The domain name of the HTTP server */
-	const uint16_t _port;           /**< The HTTP server port */
-	char _buffer[RECV_BUFFER_SIZE];	/**< The response buffer */
-	size_t _bpos;					/**< The current offset in the response buffer */
-	SocketAddr _remoteAddr;         /**< The remote address */
-	bool _got200;    				/**< Status flag for HTTP 200 */
-	bool _gothello;                 /**< Status flag for finding the test string */
-	bool _error;                    /**< Status flag for an error */
+    TCPStream _stream;              /**< The TCP Socket */
+    const char *_domain;            /**< The domain name of the HTTP server */
+    const uint16_t _port;           /**< The HTTP server port */
+    char _buffer[RECV_BUFFER_SIZE]; /**< The response buffer */
+    size_t _bpos;                   /**< The current offset in the response buffer */
+    SocketAddr _remoteAddr;         /**< The remote address */
+    bool _got200;                   /**< Status flag for HTTP 200 */
+    bool _gothello;                 /**< Status flag for finding the test string */
+    bool _error;                    /**< Status flag for an error */
 protected:
-	//TODO: Remove this section using std::function (Alpha 3)
-	CThunk<HelloHTTP> _connect, _receive, _resolved;
+    CThunk<HelloHTTP> _connect, _receive, _resolved;
 };
 
 /**
  * The main loop of the TCP Hello World test
  */
 int main() {
-	EthernetInterface eth;
-	Timer tp;      /**< Use a timer to record how long DHCP takes */
+    EthernetInterface eth;
+    eth.init(); //Use DHCP
+    eth.connect();
+    volatile int rc = lwipv4_socket_init();
+    if (rc != SOCKET_ERROR_NONE) {
+        return 1;
+    }
 
-	tp.start();
-	eth.init(); //Use DHCP
-	eth.connect();
-	tp.stop();
-	/* Generate a random port using the timer value from DHCP */
-	uint16_t localPort = (tp.read_us() % 32768) + 32768;
-	printf("TCP client IP Address is %s\n", eth.getIPAddress());
+    printf("TCP client IP Address is %s\n", eth.getIPAddress());
 
-	// TODO: Remove when yotta supports init
-	if (lwipv4_socket_init() != SOCKET_ERROR_NONE) {
-		notify_completion(false);
-	}
-
-	/* Create the HelloHTTP Instance */
-	HelloHTTP hello(HTTP_SERVER_NAME, HTTP_SERVER_PORT, SOCKET_STACK_LWIP_IPV4, localPort);
-	/* Start the test */
-	int rc = hello.startTest(HTTP_PATH);
-	if ((rc == SOCKET_ERROR_BUSY) || (rc == SOCKET_ERROR_NONE)) {
-		/* Wait for the test to complete */
-		while (!hello.done()) {
-			__WFI();
-		}
-	}
-	/* Shut down the socket and ethernet interface */
-	hello.close();
-	eth.disconnect();
-	notify_completion(!hello.error());
-	return 0;
-}
-extern "C" void HardFault_Handler() {
-	asm ("bkpt"::);
+    HelloHTTP hello(HTTP_SERVER_NAME, HTTP_SERVER_PORT);
+    rc = hello.startTest(HTTP_PATH);
+    if (rc != SOCKET_ERROR_NONE) {
+        return 1;
+    }
+    while (!hello.done()) {
+        __WFI();
+    }
+    /* Shut down the socket before the ethernet interface */
+    hello.close();
+    eth.disconnect();
+    notify_completion(!hello.error());
+    return (int)hello.error();
 }
 
--- test/helloworld-udpclient/main.cpp
+++ test/helloworld-udpclient/main.cpp
@@ -21,13 +21,13 @@
 #include <stdint.h>
 #include <string.h>
 
-// TODO: Remove when yotta supports init.
+/* TODO: Remove when yotta supports init. */
 #include "lwipv4_init.h"
 
 #define UDP_TIME_PORT 37
 
 namespace {
-     //const char *HTTP_SERVER_NAME = "utcnist.colorado.edu";
+     /* const char *HTTP_SERVER_NAME = "utcnist.colorado.edu"; */
      const char *HTTP_SERVER_NAME = "128.138.140.44";
      const float YEARS_TO_PASS = 114.0;
 }
@@ -37,24 +37,21 @@ namespace {
  */
 class UDPGetTime {
 public:
-	/**
-	 * UDPGetTime Constructor
-	 * Initializes the UDP socket, sets up event handlers and flags.
-	 *
-	 * Note that CThunk is used for event handlers.  This will be changed to a C++
-	 * function pointer in an upcoming release.
-	 *
-	 * @param[in] stack The stack to use for the UDP Socket.  Currently,
-	 * only SOCKET_STACK_LWIP_IPV4 is supported
-	 */
-    UDPGetTime(const socket_stack_t stack) :
-        sock(stack),
+    /**
+     * UDPGetTime Constructor
+     * Initializes the UDP socket, sets up event handlers and flags.
+     *
+     * Note that CThunk is used for event handlers.  This will be changed to a C++
+     * function pointer in an upcoming release.
+     */
+    UDPGetTime() :
+        sock(SOCKET_STACK_LWIP_IPV4),
         _udpTimePort(UDP_TIME_PORT),
-		_time(0),
+        _time(0),
         _recv_irq(this),
         _dns_irq(this)
-	{
-    	/* Initialize callbacks for events */
+    {
+        /* Initialize callbacks for events */
         _recv_irq.callback(&UDPGetTime::onRecv);
         _dns_irq.callback(&UDPGetTime::onDNS);
         /* Open the UDP socket so that DNS will work */
@@ -67,7 +64,6 @@ public:
      * @return SOCKET_ERROR_NONE on success, or an error code on failure
      */
     socket_error_t startGetTime(const char *address) {
-        socket_error_t err;
         /* Initialize the GetTime flags */
         resolved = false;
         received = false;
@@ -85,7 +81,7 @@ public:
      * to extract the time from UDPGetTime
      * @return The 32-bit time since 1970-01-01T00:00:00 in seconds.
      */
-    uint32_t getTime() { return _time;}
+    uint32_t time() { return _time;}
 protected:
     /**
      * The DNS Response Handler
@@ -101,32 +97,32 @@ protected:
         /* Send the query packet to the remote host */
         socket_error_t err = sock.send_to("time",strlen("time"),&_resolvedAddr,_udpTimePort);
         /* A failure on send is a fatal error in this example */
-    	if (err != SOCKET_ERROR_NONE) {
-    		printf("Socket Error %d\r\n", err);
-    	    notify_completion(false);
-    	}
+        if (err != SOCKET_ERROR_NONE) {
+            printf("Socket Error %d\r\n", err);
+            notify_completion(false);
+        }
     }
     /**
      * The Time Query response handler
      * @param[in] arg (unused)
      */
     void onRecv(void *arg) {
-    	(void) arg;
-    	/* Initialize the buffer size */
-    	size_t nRx = sizeof(_rxBuf);
-    	/* Receive some bytes */
-    	socket_error_t err = sock.recv(_rxBuf, &nRx);
+        (void) arg;
+        /* Initialize the buffer size */
+        size_t nRx = sizeof(_rxBuf);
+        /* Receive some bytes */
+        socket_error_t err = sock.recv(_rxBuf, &nRx);
         /* A failure on recv is a fatal error in this example */
-    	if (err != SOCKET_ERROR_NONE) {
-    		printf("Socket Error %d\r\n", err);
-    	    notify_completion(false);
-    	}
+        if (err != SOCKET_ERROR_NONE) {
+            printf("Socket Error %d\r\n", err);
+            notify_completion(false);
+        }
         uint32_t time;
         /* Correct for possible non 32-bit alignment */
         memcpy(&time, _rxBuf, sizeof(time));
         /* Switch to host order */
         _time = ntohl(time);
-    	received = true;
+        received = true;
     }
 
 protected:
@@ -140,46 +136,33 @@ protected:
 protected:
     char _rxBuf[32];
     /*
-	 * Note that CThunk is used for event handlers.  This will be changed to a C++
-	 * function pointer in an upcoming release.
-	 */
+     * Note that CThunk is used for event handlers.  This will be changed to a C++
+     * function pointer in an upcoming release.
+     */
     CThunk<UDPGetTime> _recv_irq;
     CThunk<UDPGetTime> _dns_irq;
 };
 
 int main() {
-    bool result = 0;
     EthernetInterface eth;
-    printf("Starting.\r\n");
-    eth.init(); //Use DHCP
-    printf("Running eth.connect()\r\n");
+    /* Initialise with DHCP, connect, and start up the stack */
+    eth.init();
     eth.connect();
-
-    // TODO: Remove when yotta supports init
     lwipv4_socket_init();
 
     printf("UDP client IP Address is %s\r\n", eth.getIPAddress());
-    UDPGetTime gt(SOCKET_STACK_LWIP_IPV4);
-    printf("Connecting to: %s:%d\r\n", HTTP_SERVER_NAME, UDP_TIME_PORT);
+
+    /* Get the current time */
+    UDPGetTime gt;
     gt.startGetTime(HTTP_SERVER_NAME);
 
-    /* Wait until the get time process has completed */
-    while (!gt.isReceived()) { __WFI(); }
+    /* Wait until a response is received */
+    while (!gt.isReceived()) {
+        __WFI();
+    }
 
-    /* Get the time */
-    uint32_t timeRes = gt.getTime();
+    printf("UDP: %lu seconds since 01/01/1900 00:00 GMT\r\n", gt.time());
 
-    /* Check that the response is sane */
-    const float years = timeRes / 60.0 / 60.0 / 24.0 / 365;
-    //printf("UDP: Received %d bytes from server %s on port %d\r\n", n, remote.get_address(), remote.get_port());
-    printf("UDP: %lu seconds since 01/01/1900 00:00 GMT ... %s\r\n", timeRes, timeRes > 0 ? "[OK]" : "[FAIL]");
-    printf("UDP: %.2f years since 01/01/1900 00:00 GMT ... %s\r\n", years, timeRes > YEARS_TO_PASS ? "[OK]" : "[FAIL]");
-
-    result = (timeRes > 0) && (timeRes > YEARS_TO_PASS);
     eth.disconnect();
-    notify_completion(result);
     return 0;
 }
-extern "C" void HardFault_Handler() {
-    asm ("bkpt"::);
-};
